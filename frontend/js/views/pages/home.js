0981fdb985d88786e0c78bae751e797657057855
trippy88 <tiago_mfm_@hotmail.com>
2025-09-08 18:33:46 +0100

    added seach bar

--- frontend/js/views/pages/home.js
+++ frontend/js/views/pages/home.js
@@ -3,10 +3,14 @@
 export default function renderHome() {
   return `
     <div class="main-container">
-      <div class="baymax-img">
+      <div class="search-container">
+        <label for="input">Search medication name:</label>
+        <input id="input" class="search-bar" type="text" placeholder="Insert medication name" />
+        <ul id="results" class="results-list"></ul>
+      </div><div class="baymax-img">
         <img src="../assets/baymax.png" alt="Baymax" />
-      </div>
-      <p>Welcome.</p>
+      
     </div>
+     
   `;
 }
