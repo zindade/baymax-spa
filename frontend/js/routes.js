016d21255e95d91cda27ee35d9713cd9ecdb6245
trippy88 <tiago_mfm_@hotmail.com>
2025-09-09 10:35:01 +0100

    edited home and aboutUs page

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -1,13 +1,14 @@
 import renderHome from "./views/pages/home.js";
 import renderLearnMore from "./views/pages/learnMore.js";
 import renderMedications from "./views/pages/medications.js";
-
+import renderAboutUs from "./views/pages/aboutUs.js";
 
 
 const routes = {
   "/": renderHome,
   "/medications": renderMedications,
   "/learnMore": renderLearnMore,
+  "/aboutUs": renderAboutUs,
 };
 
 export default routes;
--- frontend/js/views/pages/home.js
+++ frontend/js/views/pages/home.js
@@ -2,6 +2,28 @@
 
 export default function renderHome() {
   return `
+
+  <div class="home-container">
+      <div class="search-section">
+        <h1>Welcome to Baymax</h1>
+        <p>How can I assist you today ? </p>
+
+        <div class="search-box">
+          <input 
+            id="input" 
+            class="search-bar" 
+            type="text" 
+            placeholder="Insert your text..." 
+          />
+          <button class="search-btn">➤</button>
+        </div>
+
+        <ul id="results" class="results-list"></ul>
+      </div>
+    </div>
+  `;
+  
+    /*
     <div class="main-container">
       <div class="search-container">
         <label for="input">Search medication name:</label>
@@ -12,5 +34,5 @@ export default function renderHome() {
       
     </div>
      
-  `;
+  `;*/
 }
