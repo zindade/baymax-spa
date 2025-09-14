c90eda79d6c0720934dfc50bdcfa89c0aee642a9
salab3rt <salabert57@gmail.com>
2025-09-14 14:57:55 +0100

    Fix startup script

--- llava-med/startup.sh
+++ llava-med/startup.sh
@@ -7,10 +7,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate llava-env
 
 # Start controller in background
-python -m LLaVA-Med.llava.serve.controller --host 0.0.0.0 --port 10000
-
-# Give controller a second to start
-sleep 5
+python -m LLaVA-Med.llava.serve.controller --host 0.0.0.0 --port 10000 &
+sleep 2
 
 # Start model worker (this one stays in foreground)
 exec python -m LLaVA-Med.llava.serve.model_worker \
@@ -18,6 +16,6 @@ exec python -m LLaVA-Med.llava.serve.model_worker \
   --controller http://localhost:10000 \
   --port 40000 \
   --worker http://localhost:40000 \
-  --model-path ./llava-med-v1.5-mistral-7b/ \
+  --model-path ./LLaVA-Med/llava-med-v1.5-mistral-7b/ \
   --model-name llava-med \
   --multi-modal
